d0117126b89b75a4062bf469382bce8d49361960
natalypazm <pazmoralesnataly@gmail.com>
2020-11-24 17:51:15 -0500

    REPETITIVE STRUCTURES: WHILE

--- main.js
+++ main.js
@@ -84,6 +84,11 @@ const isDeveloper = ({ developer }) => nataly.developer ? console.log('IS DEVELO
 
 
 
+
+
+
+
+
 // REPETITIVE STRUCTURES: FOR
 console.log(`Al inicio del año ${nataly.nombre} pesa ${nataly.weight} kg`)
 
@@ -93,15 +98,29 @@ const DAYS_YEAR = 365
 const weightGain = persona => persona.weight += INCREASED_WEIGHT
 const slim_down = persona => persona.weight -= INCREASED_WEIGHT
 
-for (var i = 1; i <= DAYS_YEAR; i++) {
-    var random = Math.random();
-    if (random < 0.25) {
+// for (var i = 1; i <= DAYS_YEAR; i++) {
+//     var random = Math.random();
+//     if (random < 0.25) {
+//         weightGain(nataly)
+//     } else if (random < 0.5) {
+//         slim_down(nataly)
+//     }
+// }
+//console.log(`Al final del año ${nataly.nombre} pesa ${nataly.weight.toFixed(1)} kg`)
+
+
+// REPETITIVE STRUCTURES: WHILE
+const eatALot = () => Math.random() < 0.3
+const doesSport = () => Math.random() < 0.4
+const GOAL = nataly.weight - 3
+var days = 0
+while (nataly.weight > GOAL) {
+    if (eatALot()) {
         weightGain(nataly)
-    } else if (random < 0.5) {
+    }
+    if (doesSport()) {
         slim_down(nataly)
     }
+    days++
 }
-console.log(`Al final del año ${nataly.nombre} pesa ${nataly.weight.toFixed(1)} kg`)
-
-
-
+console.log(`Han pasado ${days} días para que ${nataly.nombre} baje 3kg.`)
