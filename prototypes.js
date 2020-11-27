1fa678120f085bf4e630f8fdc8e49f6d03aaa164
natalypazm <pazmoralesnataly@gmail.com>
2020-11-27 06:58:53 -0500

    OOP: PROTOTYPE INHERITANCE

--- prototypes.js
+++ prototypes.js
@@ -1,3 +1,12 @@
+//OOP: PROTOTYPE INHERITANCE
+function inheritsFrom(childPrototype, parentPrototype) {
+    var fn = function () { }
+    fn.prototype = parentPrototype.prototype
+    childPrototype.prototype = new fn
+    childPrototype.prototype.constructor = childPrototype
+}
+//OOP: PROTOTYPE INHERITANCE
+
 //OOP: HOW CLASSES WORK IN JS
 function Person(name, lastname, height) {
     this.name = name
@@ -10,11 +19,23 @@ Person.prototype.greet = function () {
 }
 Person.prototype.isTall = function () {
     if (this.height > 1.65) {
-        console.log('Is tall')
+        console.log('I am tall')
     } else {
-        console.log('Is small')
+        console.log('I am not tall')
     }
 }
 var nataly = new Person('Nataly', 'Paz', 1.66)
+
 nataly.greet()
 nataly.isTall()
+
+
+//OOP: PROTOTYPE INHERITANCE
+function developer(name, lastname) {
+    this.name = name
+    this.lastname = lastname
+}
+developer.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name} ${this.lastname} and I'm developer`)
+}
+//OOP: PROTOTYPE INHERITANCE
